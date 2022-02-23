2a2d0c25d67c1f24daad026571dbd7b8ec5f706f
Raphael velasco <raphaelvelasco2@gmail.com>
2022-02-23 15:23:38 -0300

    Fiz o Desafio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,110 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import './App.css'
+class calculator extends Component{
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  state = {
+    n1: "",
+    n2: "",
+    result:0
+  }
+
+  minus = () => {
+    let{n1, n2} = this.state
+    if((n1 && n2) !== (null || "")){
+      this.setState({
+        result: n1 - n2,
+        sinal:"-"
+      })
+    }else{
+      this.setState({
+        result: "Digite um valor nos inputs"
+      })
+    }
+  }
+  add = () => {
+    let{n1, n2} = this.state
+    if((n1 && n2) !== (null || "")){
+      this.setState({
+        result: n1 + n2,
+        sinal:"+"
+      })
+    }else{
+      this.setState({
+        result: "Digite um valor nos inputs"
+      })
+    }
+  }
+  mult = () => {
+    let{n1, n2} = this.state
+    if((n1 && n2) !== (null || "")){
+      this.setState({
+        result: n1 * n2,
+        sinal:"*"
+      })
+    }else{
+      this.setState({
+        result: "Digite um valor nos inputs"
+      })
+    }
+  }
+
+  div = () => {
+    let{n1, n2} = this.state
+    if((n1 && n2) !== (null || "")){
+      this.setState({
+        result: n1 / n2,
+        sinal:"/"
+      })
+    }else{
+      this.setState({
+        result: "Digite um valor nos inputs"
+      })
+    }
+}
+  clear = () => {
+    let {n1, n2} = this.state
+    if((n1 && n2) !== (null || "" || 0)){
+      this.setState({
+        result: 0,
+        n1: "",
+        n2: "",
+        sinal:""
+      })
+    }
+  }
+  handleChangeN1 = (event) => {
+    this.setState({
+      n1: Number( event.target.value),
+    })
+  }
+  handleChangeN2 = (event) => {
+    this.setState({
+      n2: Number(event.target.value)
+    })
+  }
+
+  render() {
+    let {result, n1, n2, sinal} = this.state
+    return(
+      <div className="container">
+        <h1>React Calc App</h1>
+        <h3>{result}</h3>
+        <input value={n1} onChange={this.handleChangeN1}   type="number" name="number 1"/>
+        <h3>{sinal}</h3>
+        <input value={n2} onChange={this.handleChangeN2}  type="number" name="number 2"/>
+        <div className="container2">
+        <button onClick={this.minus}>-</button>
+        <button onClick={this.mult}>*</button>
+        <button onClick={this.add}>+</button>
+        <button onClick={this.div}>/</button>
+        <button className="clear" onClick={this.clear}>Limpar</button>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+    
+  
+
+export default calculator
