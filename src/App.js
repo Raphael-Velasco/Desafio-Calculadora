46fd8119eda4335da9bfbe16d54e2326cef873ef
Raphael velasco <raphaelvelasco2@gmail.com>
2022-05-25 18:22:14 -0300

    Fiz o desafio

--- src/App.js
+++ src/App.js
@@ -1,110 +1,46 @@
 import React, {Component} from 'react'
 import './App.css'
-class calculator extends Component{
+class cronometro extends Component{
 
   state = {
-    n1: "",
-    n2: "",
-    result:0
+    seconds:0,
+    thousandths:0
   }
 
-  minus = () => {
-    let{n1, n2} = this.state
-    if((n1 && n2) !== (null || "")){
+  Start = () => {
+    const Stock = setInterval(() => {
       this.setState({
-        result: n1 - n2,
-        sinal:"-"
-      })
-    }else{
-      this.setState({
-        result: "Digite um valor nos inputs"
-      })
-    }
-  }
-  add = () => {
-    let{n1, n2} = this.state
-    if((n1 && n2) !== (null || "")){
-      this.setState({
-        result: n1 + n2,
-        sinal:"+"
-      })
-    }else{
-      this.setState({
-        result: "Digite um valor nos inputs"
-      })
-    }
-  }
-  mult = () => {
-    let{n1, n2} = this.state
-    if((n1 && n2) !== (null || "")){
-      this.setState({
-        result: n1 * n2,
-        sinal:"*"
-      })
-    }else{
-      this.setState({
-        result: "Digite um valor nos inputs"
-      })
-    }
-  }
+        seconds: this.state.seconds + 1
+      });
+    }, 700);
+    this.Stop = () => {
+      clearInterval(Stock);
+    };
+  };
 
-  div = () => {
-    let{n1, n2} = this.state
-    if((n1 && n2) !== (null || "")){
-      this.setState({
-        result: n1 / n2,
-        sinal:"/"
-      })
-    }else{
-      this.setState({
-        result: "Digite um valor nos inputs"
-      })
-    }
-}
-  clear = () => {
-    let {n1, n2} = this.state
-    if((n1 && n2) !== (null || "" || 0)){
-      this.setState({
-        result: 0,
-        n1: "",
-        n2: "",
-        sinal:""
-      })
-    }
-  }
-  handleChangeN1 = (event) => {
+  Restart = () => {
     this.setState({
-      n1: Number( event.target.value),
-    })
-  }
-  handleChangeN2 = (event) => {
-    this.setState({
-      n2: Number(event.target.value)
+      seconds:0
     })
   }
 
-  render() {
-    let {result, n1, n2, sinal} = this.state
+  render(){
     return(
-      <div className="container">
-        <h1>React Calc App</h1>
-        <h3>{result}</h3>
-        <input value={n1} onChange={this.handleChangeN1}   type="number" name="number 1"/>
-        <h3>{sinal}</h3>
-        <input value={n2} onChange={this.handleChangeN2}  type="number" name="number 2"/>
-        <div className="container2">
-        <button onClick={this.minus}>-</button>
-        <button onClick={this.mult}>*</button>
-        <button onClick={this.add}>+</button>
-        <button onClick={this.div}>/</button>
-        <button className="clear" onClick={this.clear}>Limpar</button>
+      <div>
+      <h1>Cronômetro</h1>
+      <div className="stopwatch-container">
+        <h2>{this.state.seconds}</h2>
+        <div className='button-container'>
+        <button onClick={this.Start}>Start</button>
+        <button onClick={this.Stop}>Stop</button>
+        <button onClick={this.Restart}>Restart</button>
         </div>
+      </div>
       </div>
     )
   }
 }
-
     
   
 
-export default calculator
+export default cronometro
